94c44cbc766261d7e5fd8dcc29393f73025f048f
joe <joeltimothyoh@live.com>
2022-10-14 02:26:58 +0000

    Refactor (ci): Rename build variables (#25)

--- build.sh
+++ build.sh
@@ -11,38 +11,37 @@
 # DOCKERHUB_REGISTRY_PASSWORD=            # string - docker hub password or api key - required
 
 ## build variables ##
-# OS=ubuntu                               # string - OS image namespace - required
-# OS_ARCHIVE=xenial-20181005              # string - OS image tag - required
-# TOOL_VARIANT=git                        # string - variant to build - required
+# BASE_IMAGE_NAME=ubuntu                               # string - OS image namespace - required
+# BASE_IMAGE_TAG=xenial-20181005              # string - OS image tag - required
+# BUILD_IMAGE_VARIANT=git                        # string - variant to build - required
 # TAG_LATEST=true                         # bool - tag image with `:latest` - optional
 # BASE_REGISTRY_NAMESPACE=library         # string - user or organization namespace of base image - required
 # BUILD_REGISTRY_NAMESPACE=startersclan   # string - user or organization namespace for build image - required
 # BUILD_IMAGE_NAME=steamcmd               # string - build image namespace - required
-# TAG_TOOL_ARCHIVE=                       # string - release tag - optional
+# RELEASE_TAG_REF=                       # string - release tag - optional
 
 #############################  End of CI variables  ##############################
 
 # Process user variables
-if [ -n "${TAG_TOOL_ARCHIVE}" ]; then
+if [ -n "${RELEASE_TAG_REF}" ]; then
     DOCKERHUB_REGISTRY_USER=${DOCKERHUB_REGISTRY_USER:?err}
     DOCKERHUB_REGISTRY_PASSWORD=${DOCKERHUB_REGISTRY_PASSWORD:?err}
 fi
 
 # Process job variables
-OS=${OS:?err}
-OS_ARCHIVE=${OS_ARCHIVE:?err}
-TOOL_VARIANT=${TOOL_VARIANT:?err}
+BASE_IMAGE_NAME=${BASE_IMAGE_NAME:?err}
+BASE_IMAGE_TAG=${BASE_IMAGE_TAG:?err}
+BUILD_IMAGE_VARIANT=${BUILD_IMAGE_VARIANT:?err}
 TAG_LATEST=${TAG_LATEST:-}
 BASE_REGISTRY_NAMESPACE=${BASE_REGISTRY_NAMESPACE:?err}
 BUILD_REGISTRY_NAMESPACE=${BUILD_REGISTRY_NAMESPACE:?err}
 BUILD_IMAGE_NAME=${BUILD_IMAGE_NAME:?err}
-TAG_TOOL_ARCHIVE=${TAG_TOOL_ARCHIVE:-}
+RELEASE_TAG_REF=${RELEASE_TAG_REF:-}
 
 # Process default job variables
-BASE_IMAGE_NAME="$OS"
-BASE_TAG_FULL="$OS_ARCHIVE"
-BUILD_TAG_FULL="$TOOL_VARIANT"
-BUILD_CONTEXT="variants/$TOOL_VARIANT"
+BASE_TAG_FULL="$BASE_IMAGE_TAG"
+BUILD_TAG_FULL="$BUILD_IMAGE_VARIANT"
+BUILD_CONTEXT="variants/$BUILD_IMAGE_VARIANT"
 
 # Display system info
 hostname
@@ -59,17 +58,16 @@ docker version
 set -e
 
 # Docker registry login
-if [ -n "${TAG_TOOL_ARCHIVE}" ]; then
+if [ -n "${RELEASE_TAG_REF}" ]; then
     echo "${DOCKERHUB_REGISTRY_PASSWORD}" | docker login -u "${DOCKERHUB_REGISTRY_USER}" --password-stdin
 fi
 
 # Print job variables
-echo "OS: $OS"
-echo "OS_ARCHIVE: $OS_ARCHIVE"
-echo "TOOL_VARIANT: $TOOL_VARIANT"
+echo "BASE_IMAGE_NAME: $BASE_IMAGE_NAME"
+echo "BASE_IMAGE_TAG: $BASE_IMAGE_TAG"
+echo "BUILD_IMAGE_VARIANT: $BUILD_IMAGE_VARIANT"
 echo "TAG_LATEST: $TAG_LATEST"
 echo "BASE_REGISTRY_NAMESPACE: $BASE_REGISTRY_NAMESPACE"
-echo "BASE_IMAGE_NAME: $BASE_IMAGE_NAME"
 echo "BASE_TAG_FULL: $BASE_TAG_FULL"
 echo "BUILD_REGISTRY_NAMESPACE: $BUILD_REGISTRY_NAMESPACE"
 echo "BUILD_IMAGE_NAME: $BUILD_IMAGE_NAME"
@@ -77,7 +75,7 @@ echo "BUILD_TAG_FULL: $BUILD_TAG_FULL"
 echo "BUILD_CONTEXT: $BUILD_CONTEXT"
 echo "BASE_IMAGE: ${BASE_REGISTRY_NAMESPACE}/${BASE_IMAGE_NAME}:${BASE_TAG_FULL}"
 echo "BUILD_IMAGE: ${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}:${BUILD_TAG_FULL}"
-echo "TAG_TOOL_ARCHIVE: $TAG_TOOL_ARCHIVE"
+echo "RELEASE_TAG_REF: $RELEASE_TAG_REF"
 
 # Build the image
 date
@@ -91,8 +89,8 @@ time docker build \
 if [ "${TAG_LATEST}" = 'true' ]; then
     docker tag "${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}:${BUILD_TAG_FULL}" "${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}:latest"
 fi
-if [ -n "${TAG_TOOL_ARCHIVE}" ]; then
-    docker tag "${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}:${BUILD_TAG_FULL}" "${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}:${TOOL_VARIANT}-${TAG_TOOL_ARCHIVE}"
+if [ -n "${RELEASE_TAG_REF}" ]; then
+    docker tag "${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}:${BUILD_TAG_FULL}" "${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}:${BUILD_IMAGE_VARIANT}-${RELEASE_TAG_REF}"
 fi
 docker images
 docker inspect "${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}:${BUILD_TAG_FULL}"
@@ -102,11 +100,11 @@ docker history "${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}:${BUILD_TAG_FULL
 docker run -t --rm --entrypoint /bin/bash "${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}:${BUILD_TAG_FULL}" -c "printenv && ls -al && exec steamcmd.sh +login anonymous +quit"
 
 # Push the image
-if [ -n "${TAG_TOOL_ARCHIVE}" ]; then
+if [ -n "${RELEASE_TAG_REF}" ]; then
     docker push -a "${BUILD_REGISTRY_NAMESPACE}/${BUILD_IMAGE_NAME}"
 fi
 
 # Docker registry logout
-if [ -n "${TAG_TOOL_ARCHIVE}" ]; then
+if [ -n "${RELEASE_TAG_REF}" ]; then
     docker logout
 fi
